c2569c9b11daf041a4bffbde875f1c5392ff0951
git <git@litianwen.cn>
2017-04-05 21:05:39 +0800

    adjust directory

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const netease = require('./netease/');
-const tecent = require('./tecent/');
-const ali = require('./ali/');
-const kuwo = require('./kuwo/');
-const kugou = require('./kugou/');
-const baidu = require('./baidu/');
-const oneTing = require('./1ting/');
-const migu = require('./migu/');
+const netease = require('./lib/netease/');
+const tecent = require('./lib/tecent/');
+const ali = require('./lib/ali/');
+const kuwo = require('./lib/kuwo/');
+const kugou = require('./lib/kugou/');
+const baidu = require('./lib/baidu/');
+const oneTing = require('./lib/1ting/');
+const migu = require('./lib/migu/');
 
 module.exports = {
 	netease,
